82244f633750a8392a9690a29ed7ff49e8e45d4e
zeal24360056@fsgcl.jp <zeal24360056@fsgcl.jp>
2024-11-01 14:22:40 +0900

    ボタンをテキストに表示

--- CS_Form/CS_Form/TestButton.cs
+++ CS_Form/CS_Form/TestButton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,15 @@ namespace CS_Form
 {
     internal class TestButton : Button
     {
-        public TestButton()
+        public TestButton(int x, int y)
         {
             //ClickイベントにOnClick関数を登録
             Click += OnClick;
+
+            //ボタン内に文字を表示させる
+            Text = "ボタン";
+
+            Location = new Point(x,y);
         }
 
         public void OnClick(object sender, EventArgs e)
